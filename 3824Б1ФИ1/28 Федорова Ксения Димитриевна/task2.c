25261a59500700941080fad1d50c51eb9ce85597
Ksenia <109479530+Ksenia555@users.noreply.github.com>
2024-12-05 10:14:48 +0300

    Update task2.c

--- 3824Б1ФИ1/28 Федорова Ксения Димитриевна/task2.c
+++ 3824Б1ФИ1/28 Федорова Ксения Димитриевна/task2.c
@@ -1,6 +1,12 @@
 // Задача 2. Найти произведение натуральных чисел a и b
 // Если a * b >= UINT_MAX, возвращать UINT_MAX
+
+#include <limits.h>
 unsigned int product_of_two_numbers(unsigned int a, unsigned int b)
 {
-  return 0;
+  unsigned long mult;
+  mult = (unsigned long)a * (unsigned long)b;
+
+  if (mult >= UINT_MAX) return UINT_MAX;
+  else return (unsigned int)mult;
 }
